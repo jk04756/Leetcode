da50fa163d519b4e433fd0939dcf989a8911b286
jk04756 <jayeshkapgate04@gmail.com>
2026-08-11 19:34:29 +0530

    Time: 1 ms (99.9%), Space: 48 MB (20.7%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+
+        int position =0;
+        for(int num: nums){
+            if(num!=0){
+                nums[position++] = num;
+            }
+
+        }
+        while(position< nums.length){
+            nums[position++]=0;
+        }
+        
+    }
+}
